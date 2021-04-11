2df4d25d85fe9f02ebe03633bdde1626fc0d8198
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-04-11 21:09:57 +0200

    Enable discovery (Eureka)

--- src/main/java/com/flymanager/api/engins/ApiEnginsApplication.java
+++ src/main/java/com/flymanager/api/engins/ApiEnginsApplication.java
@@ -2,8 +2,10 @@ package com.flymanager.api.engins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiEnginsApplication {
 
 	public static void main(String[] args) {
