a0dd097af87d59b59d2b8974e8d71fbad0e9e0c9
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-04-11 19:39:21 +0200

    Initial commit

---
+++ src/main/java/com/flymanager/api/engins/ApiEnginsApplication.java
@@ -0,0 +1,13 @@
+package com.flymanager.api.engins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEnginsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEnginsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flymanager/api/engins/ApiEnginsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flymanager.api.engins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEnginsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
