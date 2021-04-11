6d507d285662110cbf9c8fca03dbce0ca82ffcbe
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-04-11 22:00:19 +0200

    Model - Create Engin class

---
+++ src/main/java/com/flymanager/api/engins/model/Engin.java
@@ -0,0 +1,129 @@
+package com.flymanager.api.engins.model;
+
+import java.util.Date;
+
+public class Engin {
+
+	private int id;
+	private String registrationNumber;
+	private String type;
+	private String brand;
+	private String model;
+	private int available;
+	private double hourlyRate;
+	private Date creationDate;
+	private int creationUser;
+	private Date modificationDate;
+	private int modificationUser;
+
+	public Engin(int id, String registrationNumber, String type) {
+		this.id = id;
+		this.registrationNumber = registrationNumber;
+		this.type = type;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(int available) {
+		this.available = available;
+	}
+
+	public double getHourlyRate() {
+		return hourlyRate;
+	}
+
+	public void setHourlyRate(double hourlyRate) {
+		this.hourlyRate = hourlyRate;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public int getCreationUser() {
+		return creationUser;
+	}
+
+	public void setCreationUser(int creationUser) {
+		this.creationUser = creationUser;
+	}
+
+	public Date getModificationDate() {
+		return modificationDate;
+	}
+
+	public void setModificationDate(Date modificationDate) {
+		this.modificationDate = modificationDate;
+	}
+
+	public int getModificationUser() {
+		return modificationUser;
+	}
+
+	public void setModificationUser(int modificationUser) {
+		this.modificationUser = modificationUser;
+	}
+
+	@Override
+	public String toString() {
+		return "Engin{" +
+				"id=" + id +
+				", registrationNumber='" + registrationNumber + '\'' +
+				", type='" + type + '\'' +
+				", brand='" + brand + '\'' +
+				", model='" + model + '\'' +
+				", available=" + available +
+				", hourlyRate=" + hourlyRate +
+				", creationDate=" + creationDate +
+				", creationUser=" + creationUser +
+				", modificationDate=" + modificationDate +
+				", modificationUser=" + modificationUser +
+				'}';
+	}
+}
