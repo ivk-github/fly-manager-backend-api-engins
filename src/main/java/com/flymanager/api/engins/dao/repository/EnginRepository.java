f421a3f1dcb622952f2a31224a75cfcbe69f4332
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-04-17 10:33:16 +0200

    DAO - Create EnginRepository

---
+++ src/main/java/com/flymanager/api/engins/dao/repository/EnginRepository.java
@@ -0,0 +1,10 @@
+package com.flymanager.api.engins.dao.repository;
+
+import com.flymanager.api.engins.model.Engin;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface EnginRepository extends JpaRepository<Engin, Integer> {
+}
