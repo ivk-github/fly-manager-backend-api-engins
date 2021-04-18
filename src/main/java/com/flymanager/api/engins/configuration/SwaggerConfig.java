77cb50c0f854bfe107fab491c538806c344d3bcb
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-04-18 11:17:40 +0200

    API Documentation - Swagger configuration

---
+++ src/main/java/com/flymanager/api/engins/configuration/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.flymanager.api.engins.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.flymanager.api.engins"))
+				.paths(PathSelectors.regex("/engins.*"))
+				.build();
+	}
+}
