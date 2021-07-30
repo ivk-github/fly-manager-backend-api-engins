31c8c903bad9d2343db2cb0dbdc54a63a738de25
Ivan Katjounis <ivk.msg+github@gmail.com>
2021-07-30 17:38:21 +0200

    Refactor

--- src/main/java/com/flymanager/api/engins/service/EnginServiceImpl.java
+++ src/main/java/com/flymanager/api/engins/service/EnginServiceImpl.java
@@ -8,8 +8,8 @@ import com.flymanager.api.engins.mapper.EnginMapper;
 import com.flymanager.api.engins.model.Engin;
 import com.flymanager.api.engins.util.Translator;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
